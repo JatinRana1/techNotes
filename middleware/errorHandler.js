f0cc903631b59a0d4ec4ff13dabefa6e3e577722
Jatin Rana <jatinrana120@gmail.com>
2025-02-11 17:51:22 +0530

    first commit

---
+++ middleware/errorHandler.js
@@ -0,0 +1,12 @@
+const { logEvents } = require('../middleware/logger')
+
+const errorHandler = (err,req,res,next) => {
+    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errorLog.log')
+    console.log(err.stack);
+    const status = res.statusCode ? res.statusCode : 500 // Internal Server Error
+    res.status(status);
+    res.json({message: err.message})
+    next();
+}
+
+module.exports = errorHandler;
